7db34237e0a4b1193d6d3d6efaf597d4212cf3d8
marioxabel <31260739+marioxabel@users.noreply.github.com>
2023-06-21 18:21:43 -0600

    header spacing fixed

--- Scrimba-front-end/movieWatchlistv.2/index.js
+++ Scrimba-front-end/movieWatchlistv.2/index.js
@@ -1,7 +1,54 @@
-const main = document.querySelector("main")
+////////////////BORRARRR ///////////////////
+import { dummy } from "./dummyData.js"
+const contentInfo = dummy
 
-// main.innerHTML = "<p>henlo</p>"
+///////////////////////////////////////////
+
+const main = document.querySelector("main")
+let title = ""
 
 document.querySelector("form").addEventListener("submit", e => {
     e.preventDefault()
+    title = document.getElementById("search-bar").value
+    getImdbId()
 })
+
+// async function getImdbId() {
+//     const response = await fetch(`http://www.omdbapi.com/?apikey=575185a6&s=${title}`)
+//     const data = await response.json()
+//     const mediaImdbID = data.Search.map(result => result.imdbID)
+//     console.log(mediaImdbID)
+//     getContentInfo(mediaImdbID)
+// }
+
+// function getContentInfo(IDs) {
+//     const contentInfo = []
+//     IDs.map(async id => {
+//         const response = await fetch(`http://www.omdbapi.com/?apikey=575185a6&i=${id}`)
+//         const data = await response.json()
+//         contentInfo.push(data)
+//     })
+//     console.log(contentInfo)
+    
+// }
+
+console.log(contentInfo)
+
+function renderMedia() {
+    main.innerHTML = contentInfo.map(content => {
+        const {Title, Poster} = content
+        return `
+            <div class="content">
+                <img src="${Poster}">
+                <div class="content-title">
+                    <h4>${Title}</h4>
+                </div>
+            </div>`
+    }).join("")
+
+}
+
+
+//////////////////////BORRAR /////////////
+renderMedia()
+//////////////////////////////////////////

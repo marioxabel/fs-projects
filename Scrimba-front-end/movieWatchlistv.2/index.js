f30851c54dd1e264415a8f05c65108d19f9b6959
marioxabel <31260739+marioxabel@users.noreply.github.com>
2023-06-24 10:36:08 -0600

    buttons fixed

--- Scrimba-front-end/movieWatchlistv.2/index.js
+++ Scrimba-front-end/movieWatchlistv.2/index.js
@@ -1,7 +1,13 @@
-import { getImdbId, addToWatchlist, removeFromWatchlist } from "./utils.js"
+let pageContent
+let watchlist = JSON.parse(localStorage.getItem("watchlist")) 
+const main = document.querySelector("main")
 
+if (!watchlist) {
+    watchlist = []
+    localStorage.setItem("watchlist", JSON.stringify(watchlist))
+}
 
-
+// Search form
 document.querySelector("form").addEventListener("submit", e => {
     e.preventDefault()
     let title = document.getElementById("search-bar").value
@@ -9,8 +15,84 @@ document.querySelector("form").addEventListener("submit", e => {
     getImdbId(title)
 })
 
+// Get imdb ID's
+async function getImdbId(title) {
+    const response = await fetch(`http://www.omdbapi.com/?apikey=575185a6&s=${title}`)
+    const data = await response.json()
+    const mediaImdbID = data.Search.map(result => result.imdbID)
+    getContentInfo(mediaImdbID)
+}
+
+// Get the info of the ID's
+async function getContentInfo(IDs) {
+    const contentInfo = await Promise.all(
+        IDs.map(async id => {
+            const response = await fetch(`http://www.omdbapi.com/?apikey=575185a6&i=${id}`)
+            const data = await response.json()
+            return data
+        })
+    )
+    // Save contentInfo so we can use it in remove and add to watchlist
+    pageContent = contentInfo
+    renderSearchResults(contentInfo, main)
+}
+
+function renderSearchResults(contentInfo, tagToRender) {  
+    const watchlist = JSON.parse(localStorage.getItem("watchlist"))
+    tagToRender.innerHTML = contentInfo.map(content => {
+        const {Title, Poster, imdbRating, Runtime, Genre, imdbID, Plot} = content
+        // Check if title is already on watchlist and decide wich button to render
+        const button = watchlist.find(title => title.imdbID === imdbID) ? ` <button  class="remove-btn" id="remove-${imdbID}" data-imdb-ID="${imdbID}" data-rm="rm"><i class="fa-solid fa-circle-minus"></i> Remove</button>` 
+            : `<button id="add-${imdbID}" data-imdb-ID="${imdbID}" data-add="add"><i class="fa-solid fa-circle-plus"></i> Watchlist</button>`
+        
+        return `
+            <div class="content inner-container">
+                <img src="${Poster}">
+                <div class="content-text">
+                    <div class="content-title">
+                        <h3>${Title}</h3>
+                        <p class="star-icon"><i class="fa-solid fa-star"></i><p>
+                        <p>${imdbRating}</p>
+                    </div>
+                    <div class="content-info">
+                        <p>${Runtime}</p>
+                        <p>${Genre}</p>
+                        ${button}
+                    </div>
+                    <div class="content-plot">
+                        <p>${Plot}</p>
+                    </div>
+                </div>
+            </div>`
+    }).join("")
+}
 
 
+
+function addToWatchlist(ID) {
+    const watchlist = JSON.parse(localStorage.getItem("watchlist"))
+    pageContent.map(title => {
+        if (title.imdbID === ID) {
+            watchlist.push(title)
+        }
+    })
+    localStorage.setItem("watchlist", JSON.stringify(watchlist))
+    renderSearchResults(pageContent, main)
+}
+
+function removeFromWatchlist(ID) {
+    const watchlist = JSON.parse(localStorage.getItem("watchlist"))
+    watchlist.map((title, index) => {
+        if (title.imdbID === ID) {
+            watchlist.splice(index, 1)
+        }
+    })
+    localStorage.setItem("watchlist", JSON.stringify(watchlist))
+    renderSearchResults(pageContent, main)
+}
+
+
+// Remove and Add to watchlist button logic
 document.addEventListener("click", (e) => {
     if (e.target.dataset.imdbId) {
         const ID = e.target.dataset.imdbId

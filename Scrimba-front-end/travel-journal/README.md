f710e9efe67f876bb32c5bcad2a523df51893bc6
marioxabel <31260739+marioxabel@users.noreply.github.com>
2023-07-17 19:16:23 -0600

    README: travel-journal

---
+++ Scrimba-front-end/travel-journal/README.md
@@ -0,0 +1,28 @@
+# My Travel Journal
+
+## Description
+
+This was one of the first projects I made after learning React. For this project I used Vite.
+
+I created a card component that renders each place mapping on an array of data. The site is responsive.
+
+## Links
+- Live site URL: [My Travel Journal](https://travel-journal-seven-theta.vercel.app/)
+
+## Technologies
+
+- HTML
+- CSS
+- javaScript
+- React
+- Vite
+- Git
+## Author
+
+Mario Abel Garcia. Financial Engineer.
+
+
+
+### Sources 
+
+- https://www.scrimba.com/
--- Scrimba-front-end/travel-journal/src/data.js
+++ Scrimba-front-end/travel-journal/src/data.js
@@ -1,6 +1,17 @@
 const data =
 [
     {
+        id: 0,
+        title: 'Wulingyuan Scenic Area',
+        location: 'CHINA',
+        link: 'https://goo.gl/maps/3ffEJQrLg1Tr1Eyo6',
+        start: '17 May, 2020',
+        end: '8 Jun, 2020',
+        description: `Scenic’ might be an understatement here: This 100-square-mile attraction in China’s Hunan Province contains thousands of sandstone pillars that are nature’s version of skyscrapers—some even stretch taller than the Empire State Building’s midpoint.`,
+        imageURL: 'https://media.cntraveler.com/photos/5cb63a5211a45eb1f830373c/master/w_960%2Cc_limit/Wulingyuan-Scenic-Area-China_GettyImages-159228516.jpg'
+    },
+    
+    {
         id: 1,
         title: 'Mount Fuji',
         location: 'JAPAN',
@@ -13,13 +24,13 @@ const data =
     
     {
         id: 2,
-        title: 'Sydney Opera House',
-        location: 'AUSTRALIA',
-        link: 'https://goo.gl/maps/Pedvr1YmRqkJaypKA',
+        title: 'Machu Picchu',
+        location: 'PERU',
+        link: 'https://goo.gl/maps/r9nBHj9hX6uJ8TrH9',
         start: '27 May, 2021',
         end: '8 Jun, 2021',
-        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
-        imageURL: 'https://source.unsplash.com/JmuyB_LibRo'
+        description: `Machu Picchu is a 15th-century Inca citadel located in the Eastern Cordillera of southern Peru on a 2,430-meter (7,970 ft) mountain ridge. Often referred to as the "Lost City of the Incas", it is the most familiar icon of the Inca Empire.`,
+        imageURL: 'https://images.unsplash.com/photo-1526392060635-9d6019884377?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wyMDg4MDd8MHwxfHNlYXJjaHwxfHxtYWNodSUyMHBpY2NodXxlbnwwfHx8fDE2ODk2MjMxMjF8MA&ixlib=rb-4.0.3&q=80&w=1080'
     },
     
     {
@@ -31,6 +42,28 @@ const data =
         end: '18 Nov, 2021',
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
         imageURL: 'https://source.unsplash.com/3PeSjpLVtLg'
+    },
+    
+    {
+        id: 4,
+        title: 'Egyptian Pyramids',
+        location: 'Egypt',
+        link: 'https://goo.gl/maps/1ba6q5rytG9eGXiS6',
+        start: '09 Jan, 2022',
+        end: '08 Feb, 2022',
+        description: `The Egyptian pyramids are ancient masonry structures located in Egypt. Sources cite at least 118 identified "Egyptian" pyramids. Approximately 80 pyramids were built within the Kingdom of Kush, now located in the modern country of Sudan.`,
+        imageURL: 'https://images.unsplash.com/photo-1539768942893-daf53e448371?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wyMDg4MDd8MHwxfHNlYXJjaHwxfHxlZ3lwdHxlbnwwfHx8fDE2ODk2MjcwMTV8MA&ixlib=rb-4.0.3&q=80&w=1080'
+    },
+    
+    {
+        id: 5,
+        title: 'Mount Roraima',
+        location: 'VENEZUELA',
+        link: 'https://goo.gl/maps/1pDvmnA6gKYw32Wn6',
+        start: '15 Jun, 2022',
+        end: '17 Jul, 2022',
+        description: `Early European explorers declared Mount Roraima—an ancient anvil-shaped peak rising out of the rain forest where Brazil, Guyana, and Venezuela meet—inaccessible. But climb it you can, and the reward from the top is thick clouds parting to reveal a panorama of other mesas and savanna pastures.`,
+        imageURL: 'https://i.natgeofe.com/n/f76a2ece-f929-448b-b1cb-8aa390ee3b07/mount-roraima-venezuela.jpg?w=636&h=424'
     },
 ]
 

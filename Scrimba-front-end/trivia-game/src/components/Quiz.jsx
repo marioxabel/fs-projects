878437b4d285ca6553e221e2cfacd00772851044
marioxabel <31260739+marioxabel@users.noreply.github.com>
2023-08-17 14:35:01 -0600

    Quizzical: radio buttons y unwanted re-render fix

--- Scrimba-front-end/trivia-game/src/components/Quiz.jsx
+++ Scrimba-front-end/trivia-game/src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import he from 'he';
 import { shuffleArray, formatBooleanAnswer } from '../utils';
 import { nanoid } from 'nanoid'
@@ -15,6 +15,7 @@ export default function Quiz(props) {
             answer5: ""       
         }
     )
+    const [questionsElements, setQuestionElements] = useState()
     
     function handleChange(event) {
         const {name, value} = event.target
@@ -25,43 +26,43 @@ export default function Quiz(props) {
     
     console.log(selectedAnswers)
     
-    const questionsElements = props.data.map((questionData, index) => {
-        // Mix answers arrays or make array for boolean type of question
-        const possibleAnswersArray =  
-            questionData.type === "boolean" 
-                ? [true, false]
-                : shuffleArray([...questionData.incorrect_answers, questionData.correct_answer])
-        
-        // Map for generating radio buttons for each possible answer in array
-        const possibleAnswersRadioButtons = possibleAnswersArray.map(answer => {
-            // Since answer can be boolean or contain HTML entities we have to format it
-            const formattedAnswer = typeof answer === "boolean" ? formatBooleanAnswer(answer)
-                : he.decode(answer)
-        
-            return (
-                <>
-                    <label>                        
-                        <input
-                            type="radio"
-                            name={`answer${index + 1}`} 
-                            value={answer}
-                            onChange={handleChange}
-                        />
-                        {formattedAnswer}
-                    </label>
-                </>
-            )
+    // Generate question elements once
+    useEffect(() => {
+        const newQuestionsElements = props.data.map((questionData, index) => {
+            const possibleAnswersArray =  
+                questionData.type === "boolean" 
+                    ? [true, false]
+                    : shuffleArray([...questionData.incorrect_answers, questionData.correct_answer])
             
-        }) 
-        
-        return (
-            // Complete question & possible answers div
-            <div key={nanoid()}>
-                <h2>{he.decode(questionData.question)}</h2>
-                {possibleAnswersRadioButtons}
-            </div>
-        )
-    })
+            const possibleAnswersRadioButtons = possibleAnswersArray.map(answer => {
+                const formattedAnswer = typeof answer === "boolean" ? formatBooleanAnswer(answer)
+                    : he.decode(answer)
+            
+                return (
+                    <>
+                        <label>                        
+                            <input
+                                type="radio"
+                                name={`answer${index + 1}`} 
+                                value={answer}
+                                onChange={handleChange}
+                            />
+                            {formattedAnswer}
+                        </label>
+                    </>
+                )
+            }) 
+            
+            return (
+                // Complete question & possible answers 
+                <div key={nanoid()}>
+                    <h2>{he.decode(questionData.question)}</h2>
+                    {possibleAnswersRadioButtons}
+                </div>
+            )
+        })
+        setQuestionElements(newQuestionsElements)
+    },[props.data]) // only re-run if data changes
     
     return (
         <div className="Quiz">
